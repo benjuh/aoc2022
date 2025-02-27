8dd6c7e20ddaf199abd044fa3a46838524496923
Benjamin Stewart <benjuhminstewart@gmail.com>
2025-02-26 22:51:00 -0800

    feat: add day09 part 1 and 2

--- day09.py
+++ day09.py
@@ -1,22 +1,45 @@
 from util.files import *
 from util.runner import *
+import numpy as np
 import time
 
-TESTING = True
+TESTING = False
 
 lines = GetLines(9, TESTING)
-PrintLines(lines)
-def parse(lines):
-    _ = lines
-    return
+
+def solve(length):
+    knots = [(0,0) for _ in range(length)]
+    visited = set()
+    visited.add(knots[-1])
+    for line in lines:
+        sections = line.split(" ")
+        direction = sections[0]
+        steps = int(sections[1])
+        for _ in range(steps):
+            match direction:
+                case "U":
+                    knots[0] = (knots[0][0], knots[0][1] + 1)
+                case "D":
+                    knots[0] = (knots[0][0], knots[0][1] - 1)
+                case "R":
+                    knots[0] = (knots[0][0] + 1, knots[0][1])
+                case "L":
+                    knots[0] = (knots[0][0] - 1, knots[0][1])
+            for k in range(length - 1):
+                dx = knots[k][0] - knots[k + 1][0]
+                dy = knots[k][1] - knots[k + 1][1]
+                if abs(dx) > 1 or abs(dy) > 1:
+                    knots[k+1] = (knots[k+1][0] + np.sign(dx), knots[k+1][1] + np.sign(dy))
+                visited.add(knots[-1])
+    return len(visited)
 
 def part1():
-    parse(lines)
-    return 0
+    length = 2
+    return solve(length)
 
 def part2():
-    parse(lines)
-    return 0
+    length = 10
+    return solve(length)
 
 
 start1 = time.perf_counter()
