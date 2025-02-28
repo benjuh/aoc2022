4fbf89acc87dec0da36a1d620441bf34a5d24b64
Benjamin Stewart <benjuhminstewart@gmail.com>
2025-02-28 11:15:10 -0800

    feat: add day 11 part 1 and 2

--- day11.py
+++ day11.py
@@ -2,21 +2,100 @@ from util.files import *
 from util.runner import *
 import time
 
-TESTING = True
+TESTING = False
 
 lines = GetLines(11, TESTING)
-PrintLines(lines)
-def parse(lines):
-    _ = lines
-    return
+
+monkeys = []
+operations = []
+tests = []
+
+def parse(): 
+    i = 0
+    mod = 1
+    monkeys.clear()
+    operations.clear()
+    tests.clear()
+    while i < len(lines):
+        items_line = i+1
+        operation_line = i+2
+        test_line = i+3
+        if_line = i+4
+        else_line = i+5
+
+        items = lines[items_line].split(": ")[1].split(", ")
+        items = [int(x) for x in items]
+        monkeys.append(items)
+
+        ops = lines[operation_line].split("Operation: new = old ")[1].split(" ")
+        operations.append((ops[0], ops[1]))
+
+        test = int(lines[test_line].split("Test: divisible by ")[1])
+        mod *= test
+        do_if = int(lines[if_line][-1])
+        do_else = int(lines[else_line][-1])
+        tests.append((test, do_if, do_else))
+        i += 7
+
+    return mod
 
 def part1():
-    parse(lines)
-    return 0
+    _ = parse()
+    rounds = 20
+    inspections = [0] * len(monkeys)
+    for _ in range(rounds):
+        for id, items in enumerate(monkeys):
+            while len(items) > 0:
+                item = items.pop(0)
+                op, operand = operations[id]
+                new = item
+                if operand == 'old':
+                    operand = item
+                else:
+                    operand = int(operand)
+                if op == '*':
+                    new *= operand
+                elif op == '+':
+                    new += operand
+                new //= 3
+                divisor, send_to_if, send_to_else = tests[id]
+                if new % divisor == 0:
+                    monkeys[send_to_if].append(new)
+                else:
+                    monkeys[send_to_else].append(new)
+                inspections[id] += 1
+    
+    inspections.sort()
+    return inspections[-2] * inspections[-1]
 
 def part2():
-    parse(lines)
-    return 0
+    mod = parse()
+    rounds = 10000
+    inspections = [0] * len(monkeys)
+    for i in range(rounds):
+        for id, items in enumerate(monkeys):
+            while len(items) > 0:
+                item = items.pop(0)
+                op, operand = operations[id]
+                new = item
+                if operand == 'old':
+                    operand = item
+                else:
+                    operand = int(operand)
+                if op == '*':
+                    new *= operand
+                elif op == '+':
+                    new += operand
+                divisor, send_to_if, send_to_else = tests[id]
+                new %= mod
+                if new % divisor == 0:
+                    monkeys[send_to_if].append(new)
+                else:
+                    monkeys[send_to_else].append(new)
+                inspections[id] += 1
+    
+    inspections.sort()
+    return inspections[-2] * inspections[-1]
 
 
 start1 = time.perf_counter()
