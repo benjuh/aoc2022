1b47c467c5ebb3e85dc250875ef5a1ae88ab41bf
Benjamin Stewart <benjuhminstewart@gmail.com>
2025-02-28 22:29:41 -0800

    feat: finish day 13 part 1 and 2

--- day13.py
+++ day13.py
@@ -1,22 +1,52 @@
 from util.files import *
 from util.runner import *
 import time
+from ast import literal_eval
+from functools import cmp_to_key
 
-TESTING = True
+path = "data/day13.txt"
 
-lines = GetLines(13, TESTING)
-PrintLines(lines)
-def parse(lines):
-    _ = lines
-    return
+with open(path) as f:
+    content = [tuple(map(literal_eval, x)) for x in [a.split() for a in f.read().split("\n\n")]]
+
+def com(a, b):
+    if a < b:
+        return 1
+    elif a == b:
+        return 0
+    else:
+        return -1
+
+def compare(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return com(a, b)
+    
+    a = [a] if isinstance(a, int) else a
+    b = [b] if isinstance(b, int) else b
+
+    a_list = len(a)
+    b_list = len(b)
+    min_list = min(a_list, b_list)
+
+    for i in range(min_list):
+        res = compare(a[i], b[i])
+        if res == -1:
+            return -1
+        elif res == 1:
+            return 1
+
+    return com(a_list, b_list)
+
 
 def part1():
-    parse(lines)
-    return 0
+    return sum(idx for idx, (p1, p2) in enumerate(content, start=1) if compare(p1, p2) == 1)
 
 def part2():
-    parse(lines)
-    return 0
+    all = [[[2]], [[6]]]
+    for t in content:
+        all.extend(t)
+    all = sorted(all, key=cmp_to_key(compare), reverse=True)
+    return (all.index([[2]]) + 1) * (all.index([[6]]) + 1)
 
 
 start1 = time.perf_counter()
