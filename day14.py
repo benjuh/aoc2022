e3565dbe23f2e9db3b037f95ad0d15e627d1a879
Benjamin Stewart <benjuhminstewart@gmail.com>
2025-03-01 09:39:00 -0800

    feat: finish day 14 and 15

--- day14.py
+++ day14.py
@@ -1,22 +1,90 @@
 from util.files import *
 from util.runner import *
 import time
+import numpy as np
 
-TESTING = True
+TESTING = False
 
 lines = GetLines(14, TESTING)
-PrintLines(lines)
+
+ROCK = '#'
+SAND = 'o'
+AIR = '.'
+
 def parse(lines):
-    _ = lines
-    return
+    cave = np.full((318, 1000), '.')
+    bottom_depth = 0
+    for s in lines:
+        path = s.split(' -> ')
+        for i in range(0, len(path) - 1):
+            p1, p2 = path[i].split(','), path[i + 1].split(',')
+            x1, y1 = int(p1[0]), int(p1[1])
+            x2, y2 = int(p2[0]), int(p2[1])
+            points = points_on_line(x1, y1, x2, y2)
+            for (x, y) in points:
+                cave[y, x] = ROCK
+                bottom_depth = max(y, bottom_depth)
+
+    return cave, bottom_depth
+
+def points_on_line(x1, y1, x2, y2):
+    xrange = range(x1, x2 + 1) if x2 >= x1 else range(x2, x1 + 1)
+    yrange = range(y1, y2 + 1) if y2 >= y1 else range(y2, y1 + 1)
+    return [(x, y) for x in xrange for y in yrange]
+
+
 
 def part1():
-    parse(lines)
-    return 0
+    input, bottom_depth = parse(lines)
+    sand_count = 0
+    cave = input.copy()
+
+    while True:
+        sand_count += 1
+        r, c = 0, 500
+        while True:
+            if r > bottom_depth:
+                return sand_count - 1
+            elif cave[r + 1, c] == AIR:
+                r += 1
+            elif cave[r + 1, c - 1] == AIR:
+                r += 1
+                c -= 1
+            elif cave[r + 1, c + 1] == AIR:
+                r += 1
+                c += 1
+            else:
+                cave[r, c] = SAND
+                break
+
 
 def part2():
-    parse(lines)
-    return 0
+    input, bottom_depth = parse(lines)
+    sand_count = 0
+    cave = input.copy()
+    floor_depth = bottom_depth + 2
+
+    while True:
+        sand_count += 1
+        r, c = 0, 500
+        while True:
+            if r + 1 >= floor_depth:
+                cave[r, c] = SAND
+                break
+            elif cave[1, 499] == cave[1, 500] == cave[1, 501] == SAND:
+                # draw(cave)
+                return sand_count
+            elif cave[r + 1, c] == AIR:
+                r += 1
+            elif cave[r + 1, c - 1] == AIR:
+                r += 1
+                c -= 1
+            elif cave[r + 1, c + 1] == AIR:
+                r += 1
+                c += 1
+            else:
+                cave[r, c] = SAND
+                break
 
 
 start1 = time.perf_counter()
--- day15.py
+++ day15.py
@@ -2,21 +2,50 @@ from util.files import *
 from util.runner import *
 import time
 
-TESTING = True
+TESTING = False
 
 lines = GetLines(15, TESTING)
-PrintLines(lines)
-def parse(lines):
-    _ = lines
-    return
+
+def possible(x, y, sensors, beacons):
+    for sx, sy, d in sensors:
+        if abs(x - sx) + abs(y - sy) <= d and (x, y) not in beacons:
+            return False
+    return True
 
 def part1():
-    parse(lines)
-    return 0
+    sensors, beacons = parse(lines)
+    ct = 0
+    y = 2_000_000
+    for x in range(min(x-d for x, _, d in sensors), max(x+d for x, _, d in sensors)):
+        if not possible(x, y, sensors, beacons) and (x, y) not in beacons:
+            ct += 1
+    return ct
 
 def part2():
-    parse(lines)
-    return 0
+    sensors, beacons = parse(lines)
+    for sx, sy, d in sensors:
+        for dx in range(d + 2):
+            dy = (d + 1) - dx
+            for mx, my in [(-1, 1), (1, -1), (-1, -1), (1, 1)]:
+                x, y = sx + (dx * mx), sy + (dy * my)
+                if not(0 <= 4_000_000 and 0<=y<=4_000_000):
+                    continue
+                if possible(x, y, sensors, beacons):
+                    return x * 4_000_000 + y
+
+def parse(lines):
+    sensors, beacons = set(), set()
+    for line in lines:
+        parts = line.split()
+        sx, sy = int(parts[2][2:-1]), int(parts[3][2:-1])
+        bx, by = int(parts[8][2:-1]), int(parts[9][2:])
+        d = abs(sx - bx) + abs(sy - by)
+        sensors.add((sx, sy, d))
+        beacons.add((bx, by))
+    return sensors, beacons
+
+def manhattan(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
 start1 = time.perf_counter()
